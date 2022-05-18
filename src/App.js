7f23e37621ec72a4baa58d3add7d85bf62255b86
André Santos <x_sode@hotmail.com>
2022-05-18 16:11:15 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Content from './components/Content';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Header />
+        <Content />
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>
+          {' '}
+          <strong> Trybe</strong>
+          tunes
+        </h1>
+        <h2>Usuário</h2>
+      </header>
+    );
+  }
+}
+
+export default Header;
