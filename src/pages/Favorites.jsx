381cb4db85ce9143e135ee6fa3904781d74f6fe0
André Santos <x_sode@hotmail.com>
2022-05-20 22:53:36 -0300

    working on 12

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-favorites">
-        <Header />
-        <h1>Fovoritos</h1>
-      </div>
-    );
-  }
+state = {
+  favoritList: '',
+}
+
+async componentDidMount() {
+  const getSongs = await getFavoriteSongs();
+  this.setState({ favoritList: getSongs });
+}
+
+render() {
+  return (
+    <div data-testid="page-favorites">
+      <Header />
+      <h1>Fovoritos</h1>
+    </div>
+  );
+}
 }
 
 export default Favorites;
