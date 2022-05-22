16e17e43ea660d55ec149215a75999f87dd43407
André Santos <x_sode@hotmail.com>
2022-05-22 18:09:09 -0300

    requisito 12 com problemas de sicronicidade

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,25 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
 import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends React.Component {
 state = {
-  favoritList: '',
+  musicsList: '',
 }
 
-async componentDidMount() {
+componentDidMount() {
+  this.favoriteSongs();
+}
+
+componentDidUpdate() {
+  this.favoriteSongs();
+}
+
+favoriteSongs = async () => {
   const getSongs = await getFavoriteSongs();
-  this.setState({ favoritList: getSongs });
+  this.setState({ musicsList: getSongs });
 }
 
 render() {
+  const { musicsList } = this.state;
   return (
     <div data-testid="page-favorites">
       <Header />
-      <h1>Fovoritos</h1>
+      {musicsList && musicsList.map((music) => (
+        <MusicCard
+          key={ music.trackId }
+          trackName={ music.trackName }
+          previewUrl={ music.previewUrl }
+          trackId={ music.trackId }
+          musicsList={ musicsList }
+
+        />
+      ))}
     </div>
   );
 }
 }
+
+Favorites.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default Favorites;
