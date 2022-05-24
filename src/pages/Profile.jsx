3ed8d1c597fe3f675536891bb0f3b8392f90ba31
André Santos <x_sode@hotmail.com>
2022-05-24 09:28:51 -0300

    working on style

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  state = {
+    waiting: false,
+    user: '',
+  };
+
+  componentDidMount() {
+    this.loadingHeader();
+  }
+
+  loadingHeader = async () => {
+    this.setState({ waiting: true });
+    const userName = await getUser();
+    this.setState({ waiting: false, user: userName });
+  };
+
   render() {
     return (
       <div data-testid="page-profile">
